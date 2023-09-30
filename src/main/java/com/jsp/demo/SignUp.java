0c34bd92567f1e95a0afa16537de3bab64e8068b
TusharSupanekar <tsupanekar2810@gmail.com>
2023-09-30 10:48:08 +0530

    Servlet-Demo

---
+++ src/main/java/com/jsp/demo/SignUp.java
@@ -0,0 +1,24 @@
+package com.jsp.demo;
+
+import java.io.IOException;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class SignUp extends GenericServlet{
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		String name=req.getParameter("name");
+		String email=req.getParameter("email");
+		String password=req.getParameter("password");
+		
+		System.out.println(name);
+		System.out.println(email);
+		System.out.println(password);
+		
+	}
+
+}
---
+++ src/main/webapp/signup.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Sign-Up Page</title>
+</head>
+<body>
+
+	<form action="signup">
+
+		Name: <input type="text" name="name"><br><br>
+		Email: <input type="email" name="email"><br><br>
+		Password:<input type="password" name="password"><br><br> 
+		
+		<input type="submit">
+		
+	</form>
+
+</body>
+</html>
